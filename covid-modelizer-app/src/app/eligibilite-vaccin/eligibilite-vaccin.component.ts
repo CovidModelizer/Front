60255060f7fff373f22b977ba4df6d8b1a435680
da-ekchajzer <da.ekchajzer@gmail.com>
2021-04-07 16:36:53 +0200

    form vaccin

--- covid-modelizer-app/src/app/eligibilite-vaccin/eligibilite-vaccin.component.ts
+++ covid-modelizer-app/src/app/eligibilite-vaccin/eligibilite-vaccin.component.ts
@@ -1,19 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+
 
 @Component({
   selector: 'app-eligibilite-vaccin',
   templateUrl: './eligibilite-vaccin.component.html',
   styleUrls: ['./eligibilite-vaccin.component.css']
 })
-export class EligibiliteVaccinComponent implements OnInit {
+export class EligibiliteVaccinComponent implements OnInit, OnChanges {
+  
+  public age='';
+
 
   constructor() { 
+  }
 
+  ngOnChanges(): void {
+   
   }
 
   ngOnInit(): void {
     
   }
+
 
 }
 
