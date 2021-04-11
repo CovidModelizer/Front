e906c8fe6b4fb423545c77ff931347ba3c4f5b03
da-ekchajzer <da.ekchajzer@gmail.com>
2021-04-11 20:56:30 +0200

    add contributeur

---
+++ covid-modelizer-app/src/app/contributeurs/contributeurs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contributeurs',
+  templateUrl: './contributeurs.component.html',
+  styleUrls: ['./contributeurs.component.css']
+})
+export class ContributeursComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
